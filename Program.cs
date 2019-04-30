aaef85de40f1707abb5894fbe2b2a8e0b407ab4a
Tessa Sullivan <tessa.sullivan@gmail.com>
2019-04-30 13:24:08 -0700

    Added Program.cs, Startup.cs, HomeController.cs, CarsController.cs and CarDealership.csproj

--- Program.cs
+++ Program.cs
@@ -1,40 +1,20 @@
-using System;
-using System.Collections.Generic;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
-namespace Dealership 
+namespace CarDealership
 {
   public class Program
   {
-    public static void Main()
+    public static void Main(string[] args)
     {
-      Car porsche = new Car("2014 Porsche 911", 114991, 7864);
-      Car ford = new Car("2011 Ford F450", 55995, 14241);
-      Car lexus = new Car("2013 Lexus RX 350", 44700, 20000);
-      Car mercedes = new Car("Mercedes Benz CLS550", 39900, 37979);
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
 
-      List<Car> Cars = new List<Car>() { porsche, ford, lexus, mercedes };
-
-      lexus.SetPrice(2000);
-      
-      Console.WriteLine("Enter maximum price: ");
-      string stringMaxPrice = Console.ReadLine();
-      int maxPrice = int.Parse(stringMaxPrice);
-
-      List<Car> CarsMatchingSearch = new List<Car>();
-      foreach(Car automobile in Cars)
-      {
-        if (automobile.WorthBuying(maxPrice))
-        {
-          CarsMatchingSearch.Add(automobile);
-        }
-      }
-      foreach(Car automobile in CarsMatchingSearch)
-      {
-        Console.WriteLine("----------------------");
-        Console.WriteLine(automobile.GetMakeModel());
-        Console.WriteLine(automobile.GetMiles());
-        Console.WriteLine("$" + automobile.GetPrice());
-      }
+      host.Run();
     }
   }
 }
